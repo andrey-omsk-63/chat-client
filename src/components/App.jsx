3897b0acacfb51966c8994ed4a5ab15751171874
unknown <andrey_omsk@mail.ru>
2023-02-15 14:04:14 +0600

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AppRoutes from "./AppRoutes";
+
+const App = () => (
+  <div className="container">
+    <AppRoutes />
+  </div>
+);
+
+export default App;
+//51.12
